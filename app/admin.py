9a83814043b027fd874dcce8ccfc283bd14a516c
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-04-10 13:14:05 +0300

    configure admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-admin.site.site_header = 'Insta-Clone Admin'
-admin.site.site_title = 'Insta-Clone Admin Area'
-admin.site.index_title = 'Welcome to Insta-Clone admin area'
+admin.site.site_header = 'Neighborhood Admin'
+admin.site.site_title = 'Neighborhood Admin Area'
+admin.site.index_title = 'Welcome to Neighborhood admin area'
+
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Post)
