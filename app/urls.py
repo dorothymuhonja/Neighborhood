41e4536031782d1cdafe68c495d6b8df50f7db3c
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-04-10 16:17:04 +0300

    add and template for login, add url for logout

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
